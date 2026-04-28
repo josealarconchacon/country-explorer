9fe4e7bd053d3c4ebfe6b0ff61348b6f4cd0c2b5
josealarconchacon <jose.alarcon.chacon@gmail.com>
2026-04-28 17:55:57 -0400

    fetch countries by name and region

--- js/api/countriesApi.js
+++ js/api/countriesApi.js
@@ -1,5 +1,5 @@
 import { API_BASE_URL } from "../utils/constants.js";
-
+// fetches country data by country name.
 export async function fetchCountryByName(name) {
   const response = await fetch(`${API_BASE_URL}/name/${name}`);
 
@@ -9,7 +9,7 @@ export async function fetchCountryByName(name) {
 
   return response.json();
 }
-
+// fetches all countries within a specific region
 export async function fetchCountriesByRegion(region) {
   const response = await fetch(`${API_BASE_URL}/region/${region}`);
 
